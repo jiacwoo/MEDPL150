111a890c302400724b271f625788005e2013ed10
Jia <jiachang1234@gmail.com>
2026-04-28 15:13:18 -0400

    Update Script.js

--- html/Web_game/Script.js
+++ html/Web_game/Script.js
@@ -17,11 +17,6 @@ function nextLine() {
     document.getElementById("choice_2").style.display = "block";
     document.getElementById("choice_3").style.display = "block";
   }
-  else if (index < 2) {
-    document.getElementById("choice_1").style.display = "none";
-    document.getElementById("choice_2").style.display = "none";
-    document.getElementById("choice_3").style.display = "none";
-  }
 };
 
 document.getElementById("back").onclick = function(e) {
@@ -29,5 +24,10 @@ document.getElementById("back").onclick = function(e) {
   if (index > 0) {
     index--;
     document.getElementById("text").innerText = dialogue[index];
+  }
+  if (index < 2) {
+    document.getElementById("choice_1").style.display = "none";
+    document.getElementById("choice_2").style.display = "none";
+    document.getElementById("choice_3").style.display = "none";
   }
 };
