7b7a84e0ae25954f2883b18280c3f21f6ca182d9
Jia <jiachang1234@gmail.com>
2026-04-28 15:02:21 -0400

    Update Script.js

--- html/Web_game/Script.js
+++ html/Web_game/Script.js
@@ -25,10 +25,10 @@ document.getElementById("back").onclick = function(e) {
     index--;
     document.getElementById("text").innerText = dialogue[index];
   }
-  if (index > 2) {
-    document.getElementById("choice_1").style.display = "none";
-    document.getElementById("choice_2").style.display = "none";
-    document.getElementById("choice_3").style.display = "none";
-  }
 };
 
+if (index > 2) {
+  document.getElementById("choice_1").style.display = "none".;
+  document.getElementById("choice_2").style.display = "none";
+  document.getElementById("choice_3").style.display = "none";
+}
