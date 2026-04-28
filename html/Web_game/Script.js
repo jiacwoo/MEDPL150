f88ce337ee4d4c117563ae55d05698161a1b6d4e
Jia <jiachang1234@gmail.com>
2026-04-28 03:24:57 -0400

    Update Script.js

--- html/Web_game/Script.js
+++ html/Web_game/Script.js
@@ -1,1 +1,19 @@
+const dialogue = [
+  "You wake up in a dark room...",
+  "Your head hurts.",
+  "You hear something breathing nearby...",
+  "..."
+];
 
+let index = 0;
+
+const text = document.getElementById("text");
+
+function nextLine() {
+  if (index < dialogue.length - 1) {
+    index++;
+    text.innerText = dialogue[index];
+  }
+}
+
+document.addEventListener("click", nextLine);
