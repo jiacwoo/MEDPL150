b6a0bff8985672a6e41d5cd62cc17566c4cc4818
Jia <jiachang1234@gmail.com>
2026-05-04 00:47:07 -0400

    Update Scripttest.js

--- html/Web_game/Scripttest.js
+++ html/Web_game/Scripttest.js
@@ -44,13 +44,14 @@ function nextLine() {
     index++;
     text.innerText = dialogue[index];
   }
-  if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8)) {
+  
+
+
+  
+  if (index2 == 0 && (index == 0 || index == 1 || index == 2)) {
     speaker.style.display = "none";
   } 
-  else {
-    speaker.style.display = "block";
-  };
-  if (index2 == 0 && (index == 0 || index == 1 || index == 2)) {
+  else if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8)) {
     speaker.style.display = "none";
   } 
   else {
