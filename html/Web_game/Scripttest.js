a26fb98c930fab416d977b760e8580c6510e3fab
Jia <jiachang1234@gmail.com>
2026-05-04 00:42:24 -0400

    Update Scripttest.js

--- html/Web_game/Scripttest.js
+++ html/Web_game/Scripttest.js
@@ -44,10 +44,9 @@ function nextLine() {
     index++;
     text.innerText = dialogue[index];
   }
-  if (index2 == 0 && (index == 0 || index == 1 || index == 2)) {
+  if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8)) {
     speaker.style.display = "none";
   } 
-  if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8))
   else {
     speaker.style.display = "block";
   };
