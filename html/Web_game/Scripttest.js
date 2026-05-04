829e5027703fecf5b22ef990efbb8b7c06169564
Jia <jiachang1234@gmail.com>
2026-05-04 00:57:41 -0400

    Update Scripttest.js

--- html/Web_game/Scripttest.js
+++ html/Web_game/Scripttest.js
@@ -46,7 +46,7 @@ function nextLine() {
   }
   
   
-  if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 7) {
+  if (index2 == 0 && (index == 0 || index == 1 || index == 2) {
     speaker.style.display = "none";
   } 
   else if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8)) {
@@ -88,6 +88,7 @@ document.getElementById("choice_1").onclick = function(e) {
 
   index = 0;
   if (index2 == 0) {
+    speaker.style.display = "none";
     dialogue = [
       "The door creaks open",
       "You step down.",
