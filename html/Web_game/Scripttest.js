9cf1b1b23f9df48bb02d99a10330949d29134d6c
Jia <jiachang1234@gmail.com>
2026-05-04 00:58:16 -0400

    Update Scripttest.js

--- html/Web_game/Scripttest.js
+++ html/Web_game/Scripttest.js
@@ -46,7 +46,7 @@ function nextLine() {
   }
   
   
-  if (index2 == 0 && (index == 0 || index == 1 || index == 2) {
+  if (index2 == 0 && (index == 0 || index == 1 || index == 2)) {
     speaker.style.display = "none";
   } 
   else if (index2 == 1 && (index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8)) {
